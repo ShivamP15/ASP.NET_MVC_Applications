a7d8b33ece43aec9a1de35572801c0560c0dda61
Shivam Patel <shivuptl839@gmail.com>
2023-07-09 15:26:37 -0400

    Lab 4 Final Push

---
+++ Lab4/Lab4/Models/Subscription.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab4.Models
+{
+    public class Subscription
+    {
+        public int FanId { get; set; }
+
+        public string SportClubId { get; set;}
+
+        public Fan fan { get; set;}
+
+        public SportClub sportClub { get; set;}
+    }
+}
